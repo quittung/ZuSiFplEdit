5911ee04a918a0443e5a266cfb03e379eb67252d
Johannes Feltes <johannes.feltes@googlemail.com>
2017-02-25 13:30:43 +0100

    FIX: Zeige den Maßstab richtig an

--- ZuSiFplEdit/mapDraw.cs
+++ ZuSiFplEdit/mapDraw.cs
@@ -136,7 +136,7 @@ namespace ZuSiFplEdit
         {
             map.Clear(Color.White);
 
-            map.DrawString("N" + center_NS.ToString("F2") + " - E" + center_WE.ToString("F2") + " - 1:" + pixPerGrad.ToString("F1"), new Font("Verdana", 10), new SolidBrush(Color.Red), 20, map_height_p - 20);
+            map.DrawString("N" + center_NS.ToString("F2") + " - E" + center_WE.ToString("F2") + " - " + pixPerGrad.ToString("F1") + "pix/km", new Font("Verdana", 10), new SolidBrush(Color.Red), 20, map_height_p - 20);
 
             Pen pen_unselected = new Pen(Color.Black);
             Pen pen_selected = new Pen(Color.Green);
